e73c4dc87f99fbb6ac6ee6175135283f5d9fcea8
majinjin <2420582240@qq.com>
2021-07-16 17:19:11 +0800

    change some code

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,24 +1,22 @@
-create table merchant
-(
-    merchant_id   long(11)     not null primary key AUTO_INCREMENT,
-    merchant_name varchar(200) not null,
-    merchant_type varchar(200),
-    legal_person  varchar(200),
-    come_in_time  datetime
-);
+CREATE TABLE `merchant` (
+                            `merchant_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                            `merchant_name` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+                            `merchant_type` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+                            `legal_person` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+                            `come_in_time` datetime DEFAULT NULL,
+                            PRIMARY KEY (`merchant_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-create table store
-(
-    store_id       long(11)     not null primary key AUTO_INCREMENT,
-    store_name     varchar(200) not null,
-    address        varchar(200),
-    business_hours varchar(200),
-    mobile         varchar(200)
-);
+CREATE TABLE `merchant_store_rel` (
+                                      `merchant_id` int(11) NOT NULL,
+                                      `store_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-create table merchant_store_rel
-(
-    merchant_id long(11) not null,
-    store_id    long(11) not null
-);
-
+CREATE TABLE `store` (
+                         `store_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                         `store_name` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+                         `address` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+                         `business_hours` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+                         `mobile` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+                         PRIMARY KEY (`store_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
